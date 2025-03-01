ccd2c7aad5a7f608fe39fa6f25f898ab7189cb6a
Emperor <luiferrojas61@gmail.com>
2025-02-28 21:54:01 -0500

    practicas

---
+++ src/Media.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce el primer número:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Introduce el segundo número:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Introduce el tercer número:");
+        double num3 = scanner.nextDouble();
+
+        double media = (num1 + num2 + num3) / 3;
+
+        System.out.println("La media de los tres números es: " + media);
+        scanner.close();
+    }
+}
