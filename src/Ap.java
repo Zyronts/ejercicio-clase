2871cd46957bf1cf91b80e22657aae955c91ac2a
Emperor <luiferrojas61@gmail.com>
2025-03-13 20:50:21 -0500

    S

---
+++ src/Ap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+ public class Ap
+ {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Ingrese un número: ");
+         int numero = scanner.nextInt();
+         int resultado = numero + 10;
+         System.out.println("El resultado es: " + resultado);
+ 
+         scanner.close();
+     }
+ }
---
+++ src/Tienda.java
@@ -0,0 +1,31 @@
+public class Tienda {
+ 
+    public static void main(String[] args) {
+        double precioCamiseta = 25.0;
+        double precioPantalon = 30.0;
+        double descuento = 0.15;
+        double descuentoAdicional = 0.05;
+
+        // Precio después del descuento del 15%
+        double precioCamisetaDescuento = precioCamiseta * (1 - descuento);
+        double precioPantalonDescuento = precioPantalon * (1 - descuento);
+        System.out.println("El precio de la camiseta después del descuento es: $" + precioCamisetaDescuento);
+        System.out.println("El precio del pantalón después del descuento es: $" + precioPantalonDescuento);
+        double precioTotalconunDescuento = precioCamisetaDescuento + precioPantalonDescuento; 
+        System.out.println("El precio total de ambas prendas después de aplicar un descuento del 15% es: $" + precioTotalconunDescuento);
+
+        // Precio de la segunda camiseta con descuento del 15%
+        double precioSegundaCamiseta = precioCamiseta * (1 - descuento);
+        System.out.println("El precio de la segunda camiseta después del descuento es: $" + precioSegundaCamiseta);
+        // Precio de la primera camiseta con descuento adicional del 5%
+        double precioCamisetaDescuentoExtra = precioCamisetaDescuento * (1 - descuentoAdicional);
+        System.out.println("El precio de la primera camiseta con descuento  es: $" + precioCamisetaDescuentoExtra);
+        double precioTotalCamisetas = precioCamisetaDescuentoExtra + precioSegundaCamiseta;
+        System.out.println("El precio total de ambas camisetas después de aplicar el descuento adicional es: $" + precioTotalCamisetas);
+
+        // Precio total de prendas con descuento del 15% y descuento adicional del 5%
+        double precioTotal = precioPantalonDescuento + precioTotalCamisetas;
+
+        System.out.println("El precio total de todas las prendas después de aplicar ambos descuentos es: $" + precioTotal);
+    }
+}
